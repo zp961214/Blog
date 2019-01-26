94d827e4d949b059e3a49023f0fceb79db9d26f4
mail_zhangpeng@163.com <z21131400>
2019-01-26 16:03:54 +0800

    add axios date-fns

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import Element from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-export default ({ Vue: Vue }) => Vue.use(Element);
+import axios from 'axios';
+axios.defaults.baseURL =
+    process.env.NODE_ENV === 'production' ? (location.protocol === 'http:' ? 'http://www.norchant.com/' : 'https://www.norchant.com/') : 'http://127.0.0.1:7001/';
+export default ({ Vue }) => Vue.use(Element);
