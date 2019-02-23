a45b64f539de35394bf128d7ac56f4c8432cb7f5
zp961214 <853950441@qq.com>
2019-02-23 23:08:02 +0800

    增加分类页面

---
+++ docs/.vuepress/components/categories.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="categories"></div>
+        <aside-info />
+    </div>
+</template>
+
+<script>
+import asideInfo from '@theme/components/aside-info';
+export default {
+    name: 'categories',
+    components: {
+        asideInfo
+    },
+    data() {
+        return {};
+    },
+    methods: {}
+};
+</script>
+<style lang="scss" scoped></style>
