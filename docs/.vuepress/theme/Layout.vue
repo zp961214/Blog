52d2f2d49972ad82e149c28ab8f2594f3609bff2
Valor <853950441@qq.com>
2019-03-07 18:31:54 +0800

    添加回到顶部功能

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,12 +1,15 @@
 <template>
     <div class="theme-container">
-        <nav-menu :class="{MenuHidden}"/>
-        <mobileMenu/>
-        <banner-section/>
-        <page/>
+        <nav-menu :class="{ MenuHidden }" />
+        <mobileMenu />
+        <banner-section />
+        <page />
         <v-footer></v-footer>
         <vue-progress-bar></vue-progress-bar>
-    </div> 
+        <div :class="['back-to-top', { canBackTop }]" @click="backTop">
+            <i class="fa fa-arrow-up"></i>
+        </div>
+    </div>
 </template>
 <script>
 import Page from '@theme/views/page.vue';
@@ -14,6 +17,7 @@ import NavMenu from '@theme/components/nav-menu.vue';
 import mobileMenu from '@theme/components/mobile-menu';
 import bannerSection from '@theme/components/banner-section';
 import vFooter from '@theme/components/v-footer';
+import animate from '@assets/js/animate.js';
 export default {
     name: 'Layout',
     components: {
@@ -28,7 +32,8 @@ export default {
             reSet: null,
             title: '',
             MenuHidden: false,
-            scrollTop: 0
+            scrollTop: 0,
+            canBackTop: false
         };
     },
     mounted() {
@@ -36,15 +41,13 @@ export default {
         this.scrollTopInit();
         window.addEventListener('visibilitychange', this.visibilitychange);
         window.addEventListener('scroll', this.scrollHandle);
-        import('../assets/js/mouseClick.js');
+        import('../assets/js/mouseClick.js'); 
     },
     beforeDestroy() {
         window.removeEventListener('scroll', this.scrollHandle);
-        window.removeEventListener('visibilitychange', this.visibilitychange); 
+        window.removeEventListener('visibilitychange', this.visibilitychange);
     },
-    created(){
-       
-    }, 
+    created() {},
     methods: {
         visibilitychange() {
             document.hidden ? this.pageHidden() : this.pageShow();
@@ -58,15 +61,30 @@ export default {
             document.title = 'φ(>ω<*)咦，又好啦';
             this.reSet = setTimeout(this.reSetTitle, 2500);
         },
+
         reSetTitle() {
             document.title = this.title;
         },
+
         scrollHandle() {
-            const scrollTag = document.body.scrollTop ? document.body : document.documentElement;
-            const { scrollTop } = scrollTag;
+            const { el, docScrollTag } = this.getScrollTag('page-main', '.');
+            const { scrollTop } = docScrollTag;
             this.MenuHidden = scrollTop > this.scrollTop;
+            this.canBackTop = scrollTop > el.offsetTop - 20;
             this.scrollTop = scrollTop;
         },
+
+        getScrollTag(id, Selector = '#') {
+            const el = document.querySelector(Selector + id);
+            const docScrollTag = document.body.scrollTop ? document.body : document.documentElement;
+            return { el, docScrollTag };
+        },
+
+        backTop() {
+            const { docScrollTag } = this.getScrollTag();
+            animate(docScrollTag, { scrollTop: 0 });
+        },
+
         scrollTopInit() {
             const scrollTag = document.body.scrollTop ? document.body : document.documentElement;
             this.scrollTop = scrollTag.scrollTop;
@@ -82,9 +100,22 @@ export default {
 <style lang="scss" scoped>
 .theme-container {
     .MenuHidden {
-        top:-60px;
-        
+        top: -60px;
+    }
+    .back-to-top {
+        cursor: pointer;
+        position: fixed;
+        right: 50px;
+        top: -900px;
+        z-index: 2;
+        width: 70px;
+        height: 900px;
+        background: url('~@theme/images/scroll.png');
+        transition: all 0.5s ease-in-out;
+        opacity: 1;
+    }
+    .canBackTop {
+        top: -11px;
     }
 }
 </style>
-
