490fcf7c9e1708a4fc345911cea054094ebe91e5
Valor <853950441@qq.com>
2019-03-14 18:07:41 +0800

    新增评论

--- docs/.vuepress/theme/global-components/app-tip.vue
+++ docs/.vuepress/theme/global-components/app-tip.vue
@@ -10,11 +10,13 @@
                 <li><span @click="showAlipay = !showAlipay"> 支付宝扫一扫</span><img :style="AlipayStyle" src="~@theme/images/alipay_tip.png" alt="" /></li>
                 <li><span @click="showWepay = !showWepay">微信扫一扫</span><img :style="wepayStyle" src="~@theme/images/wepay_tip.png" alt="" /></li>
             </ul>
+            <div id="disqus_thread" slot="footer"></div>
         </app-container>
     </div>
 </template>
 
 <script>
+import DisqusJS from 'disqusjs';
 import appContainer from '@theme/components/app-container';
 export default {
     name: 'app-tip',
@@ -35,7 +37,18 @@ export default {
     },
     methods: {},
 
-    mounted() {},
+    mounted() {
+        new DisqusJS({
+            shortname: 'izp-me',
+            siteName: 'izp.me',
+            identifier: document.location.origin + document.location.pathname + document.location.search,
+            url: document.location.origin + document.location.pathname + document.location.search,
+            api: 'https://disqus.skk.moe/disqus/',
+            apikey: 'HplZkNQIgZwjGaxqaWErD6XyEl0hzqnV08qqfG8dhTCQRWUK6glTCw8vz12pMCM3',
+            admin: 'valor_coc',
+            adminLabel: ''
+        });
+    },
     beforeDestroy() {}
 };
 </script>
@@ -65,6 +78,10 @@ export default {
             list-style: circle;
             margin: 10px 0;
         }
+    }
+    #disqus_thread {
+        padding: 40px;
+        box-sizing: border-box;
     }
 }
 </style>
