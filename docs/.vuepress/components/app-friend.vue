26984d14d78fd4d14d6079cb974ba096c161355f
Valor <853950441@qq.com>
2019-03-29 22:06:52 +0800

    哈哈哈

---
+++ docs/.vuepress/components/app-friend.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="app-friend">
+        <app-container>
+            <h2>友链</h2>
+            <slot />
+            <div id="disqus_thread" slot="footer"></div>
+        </app-container>
+    </div>
+</template>
+
+<script>
+import DisqusJS from 'disqusjs';
+// import appContainer from '@theme/components/app-container';
+export default {
+    name: 'app-friend',
+    // components: { appContainer },
+    data() {
+        return {};
+    },
+    methods: {},
+    mounted() {
+        new DisqusJS({
+            shortname: 'izp-me',
+            siteName: 'izp.me',
+            identifier: document.location.origin + document.location.pathname + document.location.search,
+            url: document.location.origin + document.location.pathname + document.location.search,
+            api: 'https://disqus.skk.moe/disqus/',
+            apikey: 'HplZkNQIgZwjGaxqaWErD6XyEl0hzqnV08qqfG8dhTCQRWUK6glTCw8vz12pMCM3',
+            admin: 'valor_coc',
+            adminLabel: ''
+        });
+    }
+};
+</script>
+<style lang="scss" scoped>
+.app-friend {
+    h2 {
+        text-align: center;
+    }
+    #disqus_thread {
+        padding: 40px;
+        box-sizing: border-box;
+    }
+}
+</style>
